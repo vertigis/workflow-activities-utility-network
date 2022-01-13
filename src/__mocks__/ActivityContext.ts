3fb2de30c6d44c27212a8763b49146bd6e56afba
Colin Doak <cdoak@latitudegeo.com>
2022-01-13 11:45:51 -0800

    chore: remove redundant classes

--- src/__mocks__/ActivityContext.ts
+++
@@ -1,8 +0,0 @@
-import { IActivityContext } from "@geocortex/workflow/runtime/IActivityHandler";
-
-export function mockActivityContext(): IActivityContext {
-    const ctx: Partial<IActivityContext> = {
-        cancellationToken: Promise.resolve() as any,
-    };
-    return ctx as IActivityContext;
-}
