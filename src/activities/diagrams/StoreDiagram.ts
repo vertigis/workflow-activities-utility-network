09c0d30559da0eb4c1e4146e7d58c0177080b4ae
Colin Doak <75859289+cdoak-gh@users.noreply.github.com>
2023-08-15 08:38:57 -0700

    feat: Save and Update Diagram (#44)
    * fix: update older activites
    - re-order index
    - fix imports in tests
    
    * feat: activity to dynamically create a diagram
    
    * fix: submit as form post to avoid query params
    
    * feat: implemented the Apply Layout UN endpoint
    
    * fix: updated based on feedback
    
    * fix: linting issue
    
    * fix: changed url to forward slashes
    
    * feat: Store Diagram implementation
    
    * feat: Update Diagram implementation
    
    * feat: add new exports
    
    * fix: correct display names of activities

---
+++ src/activities/diagrams/StoreDiagram.ts
@@ -0,0 +1,136 @@
+import type { IActivityHandler } from "@geocortex/workflow/runtime/IActivityHandler";
+import esriRequest from "@arcgis/core/request";
+
+/** An interface that defines the inputs of the activity. */
+interface StoreDiagramInputs {
+    /**
+     * @displayName Service URL
+     * @description The URL to the ArcGIS REST service. For example, http://server/arcgis/rest/services/<serviceName>/NetworkDiagramServer.
+     * @required
+     */
+    serviceUrl: string;
+
+    /**
+     * @displayName Diagram ID
+     * @description The ID of the diagram.
+     * @required
+     */
+    diagramId: string;
+
+    /**
+     * @displayName Name
+     * @description The name of the diagram to be stored.
+     * @required
+     */
+    name: string;
+
+    /**
+     * @displayName Access
+     * @description The access right level you want to set for the stored diagram.
+     * @required
+     */
+    access:
+        | "esriDiagramPublicAccess"
+        | "esriDiagramProtectedAccess"
+        | "esriDiagramPrivateAccess"
+        | string;
+
+    /**
+     * @displayName Tag
+     * @description  One or several tags that could help querying the stored diagram in an easier way afterwards.
+     */
+    tag?: string;
+
+    /**
+     * @displayName Session Id
+     * @description The token (guid) used to lock the version.
+     */
+    sessionId?: string;
+    /**
+     * @displayName Geodatabase Version
+     * @description The geodatabase version on which the operation will be performed.
+     */
+    gdbVersion?: string;
+}
+
+/** An interface that defines the outputs of the activity. */
+interface StoreDiagramOutputs {
+    /**
+     * @description The result of the activity.
+     */
+    diagramInfo: {
+        tag: string;
+        isStored: boolean;
+        canStore: boolean;
+        canExtend: boolean;
+        isSystem: boolean;
+        creator: string;
+        creationDate: number;
+        lastUpdateBy: string;
+        lastUpdateDate: number;
+        containerMargin: number;
+        junctionCount: number;
+        edgeCount: number;
+        containerCount: number;
+        aggregationCount: number;
+        isHistorical: boolean;
+        access: string;
+        diagramExtent: __esri.Extent;
+        networkExtent: __esri.Extent;
+        name: string;
+        id: string;
+        template: string;
+    };
+    moment: number;
+}
+
+/**
+ * @displayName Store Diagram
+ * @description The Store Diagram operation is performed on a Diagram resource. The result of this operation is a Diagram JSON Information object,
+ * and the moment the store operation happens. It is used to store the temporary network diagram resource in the database.
+ * @category Utility Network
+ * @clientOnly
+ * @unsupportedApps GMV, GVH, WAB
+ */
+export default class StoreDiagram implements IActivityHandler {
+    /** Perform the execution logic of the activity. */
+    async execute(inputs: StoreDiagramInputs): Promise<StoreDiagramOutputs> {
+        const { serviceUrl, diagramId, name, access, ...other } = inputs;
+        if (!serviceUrl) {
+            throw new Error("serviceUrl is required");
+        }
+        if (!diagramId) {
+            throw new Error("diagramId is required");
+        }
+        if (!name) {
+            throw new Error("name is required");
+        }
+        if (!access) {
+            throw new Error("access is required");
+        }
+        // Remove trailing slashes
+        const normalizedUrl = serviceUrl.replace(/\/*$/, "");
+        const url = `${normalizedUrl}/diagrams/${diagramId}/store`;
+
+        const query = {
+            f: "json",
+            name,
+            access,
+            ...other,
+        };
+        const options: __esri.RequestOptions = {
+            method: "post",
+            query: query,
+            responseType: "json",
+        };
+        const response = await esriRequest(url, options);
+        const responseData = response.data;
+        const diagramInfo = responseData.diagramInfo;
+        const moment = responseData.moment;
+
+        return {
+            diagramInfo,
+            moment,
+        };
+    }
+}
